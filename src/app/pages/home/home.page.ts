02d7c1e037c7e7fae125daa94a5523683d7b6e1d
sureeebabu <sureshkumar20141@gmail.com>
2020-05-06 15:57:59 +0530

    search implemented

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,15 +9,69 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
+  public fullData = [];
+  public json = [
+    {
+      img: '../../../assets/handshake.png',
+      title: 'Consulting'
+    },
+    {
+      img: '../../../assets/diamond.png',
+      title: 'Diamonds'
+    },
+    {
+      img: '../../../assets/education.png',
+      title: 'Education'
+    },
+    {
+      img: '../../../assets/electrical.png',
+      title: 'Electrical'
+    },
+    {
+      img: '../../../assets/energy.png',
+      title: 'Energy'
+    },
+    {
+      img: '../../../assets/leaf.png',
+      title: 'Environment'
+    },
+    {
+      img: '../../../assets/financial.png',
+      title: 'Financial'
+    },
+    {
+      img: '../../../assets/government.png',
+      title: 'Government'
+    },
+    {
+      img: '../../../assets/healthcare.png',
+      title: 'Health Care'
+    },
+  ];
+
   constructor(
     private storage: Storage,
     private route: Router,
     private menuCtrl: MenuController
   ) {
     this.menuCtrl.enable(true);
-   }
+  }
 
   ngOnInit() {
+    this.fullData = this.json;
+  }
+
+  onSearch(event) {
+    console.log(event.target.value);
+    const searchTxt = event.target.value;
+    if (searchTxt !== '' && searchTxt != null && searchTxt !== undefined) {
+      this.json = this.json.filter((item) => {
+        return item.title.toLowerCase().indexOf(searchTxt.toLowerCase()) > -1;
+      });
+    } else {
+      this.json = this.fullData;
+
+    }
   }
 
   logutfn() {
