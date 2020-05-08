07cfdb5be3b94e99521831e53f676ae0f720313c
sureeebabu <sureshkumar20141@gmail.com>
2020-05-08 17:14:35 +0530

    Exit func Added

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { Router } from '@angular/router';
-import { MenuController } from '@ionic/angular';
+import { MenuController, Platform, AlertController } from '@ionic/angular';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
-
+export class HomePage implements OnInit, OnDestroy, AfterViewInit {
+  backButtonSubscription;
+  // for storing the returned subscription
   showSearchbar: boolean;
   term = '';
   public fullData = [];
@@ -54,13 +56,54 @@ export class HomePage implements OnInit {
   constructor(
     private storage: Storage,
     private route: Router,
-    private menuCtrl: MenuController
+    private menuCtrl: MenuController,
+    private platform: Platform,
+    private alertCtrl: AlertController,
+    private router: Router,
   ) {
     this.menuCtrl.enable(true);
   }
 
   ngOnInit() {
     this.fullData = this.json;
+  }
+
+  ngAfterViewInit() {
+    this.backButtonSubscription = this.platform.backButton.subscribe(() => {
+      if (this.router.url === '/home' || this.router.url === '/') {
+        navigator['app'].exitApp();
+      }
+      // this.exitFunction('Are you sure you want to Exit App ?');
+    });
+  }
+
+  ngOnDestroy() {
+    this.backButtonSubscription.unsubscribe();
+  }
+
+  async exitFunction(msg: string) {
+    const alert = await this.alertCtrl.create({
+      header: msg,
+      cssClass: 'buttonCss',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+          }
+        },
+        {
+          text: 'Okay',
+          handler: () => {
+            navigator['app'].exitApp();
+            // console.log('Confirm Okay');
+          }
+        }
+      ]
+
+    });
+
+    await alert.present();
   }
 
   onCancelSearch() {
