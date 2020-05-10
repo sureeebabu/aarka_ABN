3747cc79e27540f91ca70a323a3f9c27e63c1020
sureeebabu <sureshkumar20141@gmail.com>
2020-05-10 13:20:07 +0530

    contact us

--- src/app/pages/contactus/contactus.page.ts
+++ src/app/pages/contactus/contactus.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CallNumber } from '@ionic-native/call-number/ngx';
 @Component({
   selector: 'app-contactus',
   templateUrl: './contactus.page.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactusPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private callNumber: CallNumber,
+  ) { }
 
   ngOnInit() {
+  }
+
+  callFn() {
+    this.callNumber.callNumber('1234567890', true).then(res => console.log('Launched dialer!', res))
+      .catch(err => console.log('Error launching dialer', err));
   }
 
 }
